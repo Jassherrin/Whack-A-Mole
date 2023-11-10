a934eb34f587528e5aba7611034f2d9a41f9f5e3
Jassherrin <jassherrin@gmail.com>
2023-11-10 22:00:27 +0800

    Whack-a-mole is mostly functional at this point.

--- WhackAMole/app/src/main/java/com/example/whack_a_mole/MainActivity.java
+++ WhackAMole/app/src/main/java/com/example/whack_a_mole/MainActivity.java
@@ -1,47 +1,131 @@
 package com.example.whack_a_mole;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button[] buttons;
+    private TextView counterText;
+    private TextView catcher;
+    private Button endButton;
+    private int score = 0;
+    private Handler handler;
+    private boolean isGameRunning = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn1 = findViewById(R.id.button);
-        Button btn2 = findViewById(R.id.button2);
-        Button btn3 = findViewById(R.id.button3);
-        Button btn4 = findViewById(R.id.button4);
-        Button btn5 = findViewById(R.id.button5);
-        Button btn6 = findViewById(R.id.button6);
-        Button btn7 = findViewById(R.id.button7);
-        Button btn8 = findViewById(R.id.button8);
-        Button btn9 = findViewById(R.id.button9);
-        Button end = findViewById(R.id.button10);
-        TextView counterText = findViewById(R.id.textView);
-        TextView catcher = findViewById(R.id.moleCatcher);
+        buttons = new Button[9];
+        buttons[0] = findViewById(R.id.button);
+        buttons[1] = findViewById(R.id.button2);
+        buttons[2] = findViewById(R.id.button3);
+        buttons[3] = findViewById(R.id.button4);
+        buttons[4] = findViewById(R.id.button5);
+        buttons[5] = findViewById(R.id.button6);
+        buttons[6] = findViewById(R.id.button7);
+        buttons[7] = findViewById(R.id.button8);
+        buttons[8] = findViewById(R.id.button9);
 
-        btn5.setOnClickListener(new View.OnClickListener() {
+        endButton = findViewById(R.id.button10);
+        counterText = findViewById(R.id.textView);
+        catcher = findViewById(R.id.moleCatcher);
+        handler = new Handler();
+
+        for (Button button : buttons) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (isGameRunning) {
+                        Button clickedButton = (Button) view;
+                        if (clickedButton.getText().equals("*")) {
+                            score++;
+                            counterText.setText(String.valueOf(score));
+                            catcher.setText("You caught the mole!");
+                            clickedButton.setText("");
+                        } else {
+                            catcher.setText("Too bad!");
+                        }
+                    }
+                }
+            });
+        }
+
+        endButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btn5.setText("");
-                catcher.setText("You caught the mole!");
-
-                int rand = ThreadLocalRandom.current().nextInt(1,9);
-                btn1.setText("*");
-
-
+                isGameRunning = false;
+                if (score == 0) {
+                    counterText.setText("0");
+                    endButton.setBackgroundColor(getResources().getColor(android.R.color.holo_green_light));
+                    endButton.setText("Would you like to play again?");
+                    askToPlayAgain();
+                } else {
+                    Toast.makeText(MainActivity.this, "Game over! Your score: " + score, Toast.LENGTH_SHORT).show();
+                }
+                score = 0;
             }
         });
+
+        startGame();
+    }
+
+    private void startGame() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (isGameRunning) {
+                    resetButtons();
+                    showMole();
+                    handler.postDelayed(this, 1000); // Change the delay as needed for the speed of the game
+                }
+            }
+        }, 1000); // Change the initial delay as needed
+    }
+
+    private void resetButtons() {
+        for (Button button : buttons) {
+            button.setText("");
+        }
+    }
+
+    private void showMole() {
+        Random random = new Random();
+        int randomButtonIndex = random.nextInt(9);
+        buttons[randomButtonIndex].setText("*");
+    }
+
+    private void askToPlayAgain() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Would you like to play again?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        isGameRunning = true;
+                        endButton.setBackgroundColor(getResources().getColor(android.R.color.holo_purple)); // Set back to the original color
+                        endButton.setText("End game");
+                        startGame();
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                        finish();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
